60a6a8f0ad66e7f1da978002aedbefa7d534caf8
Ist-Zhy <91948294+Ist-Zhy@users.noreply.github.com>
2023-10-10 15:35:12 +0800

    Add files via upload

---
+++ FusionNetwork.py
@@ -0,0 +1,292 @@
+# coding:utf-8
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+def position(H, W, is_cuda=True):
+    if is_cuda:
+        loc_w = torch.linspace(-1.0, 1.0, W).cuda().unsqueeze(0).repeat(H, 1)
+        loc_h = torch.linspace(-1.0, 1.0, H).cuda().unsqueeze(1).repeat(1, W)
+    else:
+        loc_w = torch.linspace(-1.0, 1.0, W).unsqueeze(0).repeat(H, 1)
+        loc_h = torch.linspace(-1.0, 1.0, H).unsqueeze(1).repeat(1, W)
+    loc = torch.cat([loc_w.unsqueeze(0), loc_h.unsqueeze(0)], 0).unsqueeze(0)
+    return loc
+def stride(x, stride):
+    b, c, h, w = x.shape
+    return x[:, :, ::stride, ::stride]
+
+def init_rate_half(tensor):
+    if tensor is not None:
+        tensor.data.fill_(0.5)
+
+def init_rate_0(tensor):
+    if tensor is not None:
+        tensor.data.fill_(0.)
+
+class ACmix(nn.Module):
+    def __init__(self, in_planes, out_planes, kernel_att=7, head=4, kernel_conv=3, stride=1, dilation=1):
+        super(ACmix, self).__init__()
+        self.in_planes = in_planes
+        self.out_planes = out_planes
+        self.head = head
+        self.kernel_att = kernel_att
+        self.kernel_conv = kernel_conv
+        self.stride = stride
+        self.dilation = dilation
+        self.rate1 = torch.nn.Parameter(torch.Tensor(1))
+        self.rate2 = torch.nn.Parameter(torch.Tensor(1))
+        self.head_dim = self.out_planes // self.head
+        self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=1)
+        self.conv2 = nn.Conv2d(in_planes, out_planes, kernel_size=1)
+        self.conv3 = nn.Conv2d(in_planes, out_planes, kernel_size=1)
+        self.conv_p = nn.Conv2d(2, self.head_dim, kernel_size=1)
+        self.padding_att = (self.dilation * (self.kernel_att - 1) + 1) // 2
+        # 利用输入边界的反射来填充输入张量。
+        self.pad_att = torch.nn.ReflectionPad2d(self.padding_att)
+        # 滑动窗口，将输入根据窗口分块
+        self.unfold = nn.Unfold(kernel_size=self.kernel_att, padding=0, stride=self.stride)
+        self.softmax = torch.nn.Softmax(dim=1)
+        self.fc = nn.Conv2d(3 * self.head, self.kernel_conv * self.kernel_conv, kernel_size=1, bias=False)
+        self.dep_conv = nn.Conv2d(self.kernel_conv * self.kernel_conv * self.head_dim, out_planes,
+                                  kernel_size=self.kernel_conv, bias=True, groups=self.head_dim, padding=1,
+                                  stride=stride)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        init_rate_half(self.rate1)
+        init_rate_half(self.rate2)
+        kernel = torch.zeros(self.kernel_conv * self.kernel_conv, self.kernel_conv, self.kernel_conv)
+        for i in range(self.kernel_conv * self.kernel_conv):
+            kernel[i, i // self.kernel_conv, i % self.kernel_conv] = 1.
+        kernel = kernel.squeeze(0).repeat(self.out_planes, 1, 1, 1)
+        self.dep_conv.weight = nn.Parameter(data=kernel, requires_grad=True)
+        self.dep_conv.bias = init_rate_0(self.dep_conv.bias)
+
+    def forward(self, x):
+        q, k, v = self.conv1(x), self.conv2(x), self.conv3(x)
+        scaling = float(self.head_dim) ** -0.5
+        b, c, h, w = q.shape
+        h_out, w_out = h // self.stride, w // self.stride
+        pe = self.conv_p(position(h, w, x.is_cuda))
+        q_att = q.contiguous().view(b * self.head, self.head_dim, h, w) * scaling
+        k_att = k.contiguous().view(b * self.head, self.head_dim, h, w)
+        v_att = v.contiguous().view(b * self.head, self.head_dim, h, w)
+        if self.stride > 1:
+            q_att = stride(q_att, self.stride)
+            q_pe = stride(pe, self.stride)
+        else:
+            q_pe = pe
+        unfold_k = self.unfold(self.pad_att(k_att)).view(b * self.head, self.head_dim,
+                                                         self.kernel_att * self.kernel_att, h_out,
+                                                         w_out)
+        unfold_rpe = self.unfold(self.pad_att(pe)).view(1, self.head_dim, self.kernel_att * self.kernel_att, h_out,
+                                                         w_out)
+        att = (q_att.unsqueeze(2) * (unfold_k + q_pe.unsqueeze(2) - unfold_rpe)).sum(
+            1)
+        att = self.softmax(att)
+        out_att = self.unfold(self.pad_att(v_att)).view(b * self.head, self.head_dim, self.kernel_att * self.kernel_att,
+                                                        h_out, w_out)
+        out_att = (att.unsqueeze(1) * out_att).sum(2).view(b, self.out_planes, h_out, w_out) # 最后的输出
+        f_all = self.fc(torch.cat(
+            [q.view(b, self.head, self.head_dim, h * w), k.view(b, self.head, self.head_dim, h * w),
+             v.view(b, self.head, self.head_dim, h * w)], 1)) # 全连接
+        f_conv = f_all.permute(0, 2, 1, 3).reshape(x.shape[0], -1, x.shape[-2], x.shape[-1])
+        out_conv = self.dep_conv(f_conv)
+        return self.rate1 * out_att + self.rate2 * out_conv
+
+class ConvBnLeakyRelu2d(nn.Module):
+    # convolution
+    # batch normalization
+    # leaky relu
+    def __init__(self, in_channels, out_channels, kernel_size=3, padding=1, stride=1, dilation=1, groups=1):
+        super(ConvBnLeakyRelu2d, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding, stride=stride, dilation=dilation, groups=groups)
+        self.bn = nn.BatchNorm2d(out_channels)
+    def forward(self, x):
+        return F.leaky_relu(self.conv(x), negative_slope=0.2)
+
+class ConvBnTanh2d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, padding=1, stride=1, dilation=1, groups=1):
+        super(ConvBnTanh2d, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size,
+                              padding=padding, stride=stride, dilation=dilation, groups=groups)
+        self.bn = nn.BatchNorm2d(out_channels)
+    def forward(self, x):
+        return torch.tanh(self.conv(x))/2+0.5
+
+class ConvLeakyRelu2d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, padding=1, stride=1, dilation=1, groups=1):
+        super(ConvLeakyRelu2d, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size,
+                              padding=padding,stride=stride, dilation=dilation, groups=groups)
+    def forward(self, x):
+        return F.leaky_relu(self.conv(x), negative_slope=0.2)
+
+class Sobelxy(nn.Module):
+    def __init__(self,channels, kernel_size=3, padding=1, stride=1, dilation=1, groups=1):
+        super(Sobelxy, self).__init__()
+        sobel_filter = np.array([[1, 0, -1],
+                                 [2, 0, -2],
+                                 [1, 0, -1]])
+        self.convx=nn.Conv2d(channels, channels, kernel_size=kernel_size, padding=padding, stride=stride, dilation=dilation, groups=channels,bias=False)
+        self.convx.weight.data.copy_(torch.from_numpy(sobel_filter))
+        self.convy=nn.Conv2d(channels, channels, kernel_size=kernel_size, padding=padding, stride=stride, dilation=dilation, groups=channels,bias=False)
+        self.convy.weight.data.copy_(torch.from_numpy(sobel_filter.T))
+    def forward(self, x):
+        sobelx = self.convx(x)
+        sobely = self.convy(x)
+        x=torch.abs(sobelx) + torch.abs(sobely)
+        return x
+
+class Conv1(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=1, padding=0, stride=1, dilation=1, groups=1):
+        super(Conv1, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding, stride=stride, dilation=dilation, groups=groups)
+    def forward(self, x):
+        return self.conv(x)
+
+class SWGD(nn.Module):
+    def __init__(self, in_planes, out_planes, kernel_att=7, head=4, kernel_conv=3, stride=1, dilation=1):
+        super(SWGD, self).__init__()
+        self.in_planes = in_planes
+        self.out_planes = out_planes
+        self.head = head
+        self.kernel_att = kernel_att
+        self.kernel_conv = kernel_conv
+        self.stride = stride
+        self.dilation = dilation
+        self.rate1 = torch.nn.Parameter(torch.Tensor(1))
+        self.rate2 = torch.nn.Parameter(torch.Tensor(1))
+        self.head_dim = self.out_planes // self.head
+        self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=1)
+        self.conv2 = nn.Conv2d(in_planes, out_planes, kernel_size=1)
+        self.conv3 = nn.Conv2d(in_planes, out_planes, kernel_size=1)
+        self.conv_p = nn.Conv2d(2, self.head_dim, kernel_size=1)
+
+        self.padding_att = (self.dilation * (self.kernel_att - 1) + 1) // 2
+        self.pad_att = torch.nn.ReflectionPad2d(self.padding_att)
+        self.unfold = nn.Unfold(kernel_size=self.kernel_att, padding=0, stride=self.stride)
+        self.softmax = torch.nn.Softmax(dim=1)
+
+        self.fc = nn.Conv2d(3 * self.head, self.kernel_conv * self.kernel_conv, kernel_size=1, bias=False)
+        self.dep_conv = nn.Conv2d(self.kernel_conv * self.kernel_conv * self.head_dim, out_planes,
+                                  kernel_size=self.kernel_conv, bias=True, groups=self.head_dim, padding=1,
+                                  stride=stride)
+        self.Sobel_xy = Sobelxy(in_planes)
+        self.up = Conv1(in_planes, out_planes)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        init_rate_half(self.rate1)
+        init_rate_half(self.rate2)
+        kernel = torch.zeros(self.kernel_conv * self.kernel_conv, self.kernel_conv, self.kernel_conv)
+        for i in range(self.kernel_conv * self.kernel_conv):
+            kernel[i, i // self.kernel_conv, i % self.kernel_conv] = 1.
+        kernel = kernel.squeeze(0).repeat(self.out_planes, 1, 1, 1)
+        self.dep_conv.weight = nn.Parameter(data=kernel, requires_grad=True)
+        self.dep_conv.bias = init_rate_0(self.dep_conv.bias)
+
+    def forward(self, x):
+        q, k, v = self.conv1(x), self.conv2(x), self.conv3(x)
+        scaling = float(self.head_dim) ** -0.5
+        b, c, h, w = q.shape
+        h_out, w_out = h // self.stride, w // self.stride
+        pe = self.conv_p(position(h, w, x.is_cuda))
+        q_att = q.contiguous().view(b * self.head, self.head_dim, h, w) * scaling
+        k_att = k.contiguous().view(b * self.head, self.head_dim, h, w)
+        v_att = v.contiguous().view(b * self.head, self.head_dim, h, w)
+        if self.stride > 1:
+            q_att = stride(q_att, self.stride)
+            q_pe = stride(pe, self.stride)
+        else:
+            q_pe = pe
+        unfold_k = self.unfold(self.pad_att(k_att)).view(b * self.head, self.head_dim,
+                                                         self.kernel_att * self.kernel_att, h_out,
+                                                         w_out)
+        unfold_rpe = self.unfold(self.pad_att(pe)).view(1, self.head_dim, self.kernel_att * self.kernel_att, h_out,
+                                                        w_out)
+        att = (q_att.unsqueeze(2) * (unfold_k + q_pe.unsqueeze(2) - unfold_rpe)).sum(1)
+        att = self.softmax(att)
+
+        out_att = self.unfold(self.pad_att(v_att)).view(b * self.head, self.head_dim, self.kernel_att * self.kernel_att,
+                                                        h_out, w_out)
+        out_att = (att.unsqueeze(1) * out_att).sum(2).view(b, self.out_planes, h_out, w_out)  # 最后的输出
+        edge_x = self.Sobel_xy(x)
+        edge_result = self.up(edge_x)
+        return F.leaky_relu(edge_result+out_att, negative_slope=0.1)
+
+class SWGDBolck(nn.Module):
+    def __init__(self, channels):
+        super(SWGDBolck, self).__init__()
+        self.conv1 = SWGD(channels, channels)
+        self.conv2 = SWGD(2*channels, channels)
+    def forward(self, x):
+        x = torch.cat((x, self.conv1(x)), dim=1)
+        x = torch.cat((x, self.conv2(x)), dim=1)
+        return x
+
+class RDAB(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(RDAB, self).__init__()
+        self.dense = SWGDBolck(in_channels)
+        self.convdown = Conv1(3*in_channels, out_channels)
+        self.convup = Conv1(in_channels, out_channels)
+    def forward(self, x):
+        x1 = self.dense(x)
+        x1 = self.convdown(x1)
+        x2 = self.convup(x)
+        return F.leaky_relu(x1+x2, negative_slope=0.1)
+
+class FusionNet(nn.Module):
+    def __init__(self, output):
+        super(FusionNet, self).__init__()
+        vis_ch = [8, 16, 48]
+        inf_ch = [16, 32, 48]
+        output = 1
+        self.vis_con = ACmix(1, 8)
+        self.vis_con_1 = RDAB(8, 16)
+        self.vis_con_2 = ConvLeakyRelu2d(16, 48)
+        self.inf_con = ACmix(1, 8)
+        self.inf_con_1 = RDAB(8, 16)
+        self.inf_con_2 = ConvLeakyRelu2d(16, 48)
+        self.decode4 = ConvBnLeakyRelu2d(vis_ch[2]+inf_ch[2], vis_ch[1]+inf_ch[1])
+        self.decode3 = ConvBnLeakyRelu2d(vis_ch[1]+inf_ch[1], vis_ch[0]+inf_ch[0])
+        self.decode2 = ConvBnLeakyRelu2d(vis_ch[0]+inf_ch[0], vis_ch[0])
+        self.decode1 = ConvBnTanh2d(vis_ch[0], output)
+
+    def forward(self, image_vis, image_ir):
+        # split data into RGB and INF
+        x_vis_origin = image_vis[:, :1]
+        x_inf_origin = image_ir
+
+        # Feature extraction
+        x_vis_p = self.vis_con(x_vis_origin)
+        x_vis_p1 = self.vis_con_1(x_vis_p)
+        x_vis_p2 = self.vis_con_2(x_vis_p1)
+        x_inf_p = self.inf_con(x_inf_origin)
+        x_inf_p1 = self.inf_con_1(x_inf_p)
+        x_inf_p2 = self.inf_con_2(x_inf_p1)
+
+        # Feature reconstruction
+        x = self.decode4(torch.cat((x_vis_p2, x_inf_p2), dim=1))
+        x = self.decode3(x)
+        x = self.decode2(x)
+        x = self.decode1(x)
+        return x
+
+def unit_test():
+    import numpy as np
+    x1 = torch.tensor(np.random.rand(2, 4, 480, 640).astype(np.float32))
+    x2 = torch.tensor(np.random.rand(2, 1, 480, 640).astype(np.float32))
+    model = FusionNet(output=1)
+    y = model(x1, x2)
+    print('output shape:', y.shape)
+    assert y.shape == (2, 1, 480, 640), 'output shape (2,1,480,640) is expected!'
+    print('test ok!')
+
+if __name__ == "__main__":
+    import numpy as np
+    unit_test()
+
